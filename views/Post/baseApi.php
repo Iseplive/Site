07fa5504b34303c6c3bb4e5ec8dd4f15a73e80e4
Jean-Baptiste Watenberg <jeanbaptiste.watenberg@gmail.com>
2015-02-07 13:11:43 +0100

    debug api

--- views/Post/baseApi.php
+++ views/Post/baseApi.php
@@ -3,7 +3,7 @@ $posted = array_merge($posts,$official_posts);
 /*usort($posted, function($a, $b) {
     return $a['id'] - $b['id'];
 });*/
-json_encode($posted);
+json_encode($official_posts);
 //print_r($post);
 //require dirname(__FILE__).'/../_includes/JSON_POST.php';
 ?>
