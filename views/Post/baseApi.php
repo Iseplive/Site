e2243a8ecbedab24562746b7849fe7d85b7897cf
Jean-Baptiste Watenberg <jeanbaptiste.watenberg@gmail.com>
2015-02-07 12:29:53 +0100

    test api

--- views/Post/baseApi.php
+++ views/Post/baseApi.php
@@ -1,10 +1,3 @@
-{"posts":[
-<?php
-$ijklmn = 0;
-foreach($posts as $post){
-    require dirname(__FILE__).'/../_includes/JSON_POST.php';
-    $ijklmn++;
-    if ($ijklmn!=count($posts)) {?>,<?php}
-}
+<?php $post = $posts[0];
+require dirname(__FILE__).'/../_includes/JSON_POST.php';
 ?>
-]}
