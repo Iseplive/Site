f0c3770bdbb8f42b2cad8ed3a680fd6358858751
Jean-Baptiste Watenberg <jeanbaptiste.watenberg@gmail.com>
2015-02-11 19:18:38 +0100

    Improve Media api

--- views/Media/api.php
+++ views/Media/api.php
@@ -1,7 +1,33 @@
 <?php
-if (isset($mediaannee)) {
-    echo json_encode($mediaannee);
-}
+
 if (isset($errors)&&!empty($errors)) {
 echo json_encode(array("error"=>$errors));
 }
+?>
+<?php
+$arr = array();
+for($i=0;$i<count($annee);$i++){
+    $arrA = array();
+    for($a=0;$a<=count($mediaannee[''.$annee[$i].'']);$a++){
+        if(isset($mediaannee[''.$annee[$i].''][$a]) ){
+            $id=$mediaannee[''.$annee[$i].''][$a];
+            if(isset($mediamessage[$id]) && isset($categorie[$id])){
+                $route=Config::URL_ROOT . Routes::getPage('post', array('id' => $id));
+                $title=str_split($mediamessage[$id],35);
+                $category=$categorie[$id];
+                if(isset($title[1]) && $title[1]!=""){ $etc="...";}
+                else{ $etc="";}
+                $spanline=round(count($mediaannee[''.$annee[$i].''])/3);
+                $arrA["category_id"] = $category;
+                $arrA["id"] = $id;
+                $arrA["title"] = $title;
+            }
+        }
+    }
+    $arr[$annee[$i]] = $arrA;
+    ?>
+
+<?php
+}
+echo json_encode($arr);
+?>
